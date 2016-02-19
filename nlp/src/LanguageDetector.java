a83f438b5d6c34e3b55385eb7ffca4c4a3db51f6
LiZuyao <lizuyao2010@LiZuyaos-MacBook-Pro.local>
2016-02-19 10:33:17 -0800

    finish

--- nlp/src/LanguageDetector.java
+++ nlp/src/LanguageDetector.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.util.*;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
- * Created by lizuyao on 2/22/15.
+ * Created by lizuyao on 2/19/16.
  */
 public class LanguageDetector {
     private List <LangProfile> langProfiles;
@@ -41,6 +42,68 @@ public class LanguageDetector {
         return grams;
 
     }
+    public static void train(String folderPath, String modelPath)
+    {
+        final long threshold=10;
+        File folder = new File(folderPath);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+                BufferedReader br = null;
+                try {
+                    br = new BufferedReader(new FileReader(folderPath+"/"+listOfFiles[i].getName()));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                String line=null;
+                JSONObject obj=new JSONObject();
+
+                Map<String,Long> counter = new HashMap<>();
+                long[] n_words=new long[3];
+                try {
+                    while ( (line=br.readLine()) != null) {
+                        int n = line.length();
+                        for (int order=1; order<4; order++) {
+                            for (int j = 0; j + order - 1 < n; j++) {
+                                String gram = line.substring(j, j + order);
+                                if (!counter.containsKey(gram))
+                                    counter.put(gram, (long) 0);
+                                counter.put(gram,counter.get(gram)+1);
+                            }
+                        }
+                    }
+                    for(Iterator<Map.Entry<String, Long>> it = counter.entrySet().iterator(); it.hasNext(); ) {
+                        Map.Entry<String, Long> entry = it.next();
+                        if(entry.getValue()<threshold) {
+                            it.remove();
+                        }
+                        else
+                        {
+                            n_words[entry.getKey().length()-1]+=entry.getValue();
+                        }
+                    }
+                    obj.put("freq",counter);
+                    obj.put("name",listOfFiles[i].getName());
+                    JSONArray n_words_json = new JSONArray();
+                    for (int j=0;j<3;j++)
+                        n_words_json.add(j,n_words[j]);
+                    obj.put("n_words",n_words_json);
+                    try (FileWriter file = new FileWriter(modelPath+"/"+listOfFiles[i].getName())) {
+                        file.write(obj.toJSONString());
+                        System.out.println("Successfully dumped model File "+listOfFiles[i].getName());
+//                        System.out.println("\nJSON Object: " + obj);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+    }
+
     public String predict(String text,int order)
     {
         List<List<String>> grams=getFeatures(text);
@@ -74,17 +137,114 @@ public class LanguageDetector {
         //return predictList.get(0).getLabel();
         return bestLabel;
     }
-    public static void main(String[] args) throws IOException, ParseException {
-        final String modelfile="nlp/lang"; // directory contains pretrained model
-        LanguageDetector languageDetector =new LanguageDetector(modelfile);
+    public static void test(String modelfile, String folderPath, int order){
+        LanguageDetector languageDetector = null;
+        try {
+            languageDetector = new LanguageDetector(modelfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        File folder = new File(folderPath);
+        File[] listOfFiles = folder.listFiles();
+        int overall_correct=0;
+        int overall_total=0;
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+                BufferedReader br = null;
+                try {
+                    br = new BufferedReader(new FileReader(folderPath+"/"+listOfFiles[i].getName()));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                String line=null;
+                int correct=0;
+                int total=0;
+                try {
+                    while ( (line=br.readLine()) != null) {
+                        String predict_label = languageDetector.predict(line, order);
+                        if (predict_label.equals(listOfFiles[i].getName())) {
+                            correct++;
+                            overall_correct++;
+                        }
+                        else {
+//                            System.out.println(line);
+//                            System.out.println(predict_label);
+                        }
+                        total++;
+                        overall_total++;
+                    }
+                    System.out.println("Accuracy: "+(double)correct/total);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+        System.out.println("Total Accuracy: "+(double)overall_correct/overall_total);
+    }
+    public void testWithStdin(String modelPath)
+    {
+        LanguageDetector languageDetector = null;
+        try {
+            languageDetector = new LanguageDetector(modelPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line=null;
+        String line = null;
         String s;
         s = "";
-        while ( (line=br.readLine()) != null) {
-            s += line;
+        try {
+            while ((line = br.readLine()) != null) {
+                s += line;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        String predict_label= languageDetector.predict(s,2);
-        System.out.println("predicted language: "+predict_label);
+        String predict_label = languageDetector.predict(s, 2);
+        System.out.println("predicted language: " + predict_label);
+    }
+    public static void main(String[] args) throws IOException, ParseException {
+        String modelPath="nlp/model";
+        String phase="test";
+        String trainFolder="nlp/TrainData";
+        String testFolder="nlp/TestData";
+        int order=2;
+        if (args.length == 0) {
+            System.out.println("run default setting to train!");
+        } else {
+            for (int i=0;i<args.length;i++)
+            {
+                if (i==0)
+                    phase=args[i];
+                else if(i==1)
+                    modelPath=args[i];
+                else if(i==2) {
+                    if (phase.equals("train"))
+                        trainFolder = args[i];
+                    else
+                        testFolder = args[i];
+                }
+                else if(i==3 && phase.equals("test")) {
+                    order=Integer.parseInt(args[i]);
+                }
+            }
+        }
+        if (phase.equals("test")) {
+            try {
+                test(modelPath,testFolder,order);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else if (phase.equals("train")){
+            train(trainFolder, modelPath);
+        }
     }
 }
--- nlp/src/LanguageDetectorTest.java
+++ nlp/src/LanguageDetectorTest.java
@@ -1,28 +1,65 @@
+
 import org.junit.Test;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-
-import static org.junit.Assert.*;
 
 public class LanguageDetectorTest {
 
-    @Test
-    public void testPredict() throws Exception {
-        final String modelfile="nlp/lang"; // directory contains pretrained model
-        LanguageDetector languageDetector =new LanguageDetector(modelfile);
-        BufferedReader br = new BufferedReader(new FileReader("nlp/test/europarl.test"));
-        String line=null;
-        int correct=0;
-        int total=0;
-        while ( (line=br.readLine()) != null) {
-            String[] example = line.trim().split("\t");
-            String predict_label = languageDetector.predict(example[1], 2);
-            if (predict_label.equals(example[0]))
-                    correct++;
-            total++;
-        }
-        System.out.println("Accuracy: "+(double)correct/total);
-    }
+//    @Test
+//    public void testPredictLang() throws Exception {
+//        final String modelfile="nlp/lang"; // directory contains pretrained model
+//        LanguageDetector languageDetector =new LanguageDetector(modelfile);
+//        BufferedReader br = new BufferedReader(new FileReader("nlp/test/europarl.test"));
+//        String line=null;
+//        int correct=0;
+//        int total=0;
+//        while ( (line=br.readLine()) != null) {
+//            String[] example = line.trim().split("\t");
+//            String predict_label = languageDetector.predict(example[1], 2);
+//            if (predict_label.equals(example[0]))
+//                    correct++;
+//            total++;
+//        }
+//        System.out.println("Accuracy: "+(double)correct/total);
+//    }
+//    @Test
+//    public void testPredictModel() throws Exception {
+//        final String modelfile="nlp/model"; // directory contains pretrained model
+//        LanguageDetector languageDetector =new LanguageDetector(modelfile);
+//        String folderPath="nlp/testData";
+//        File folder = new File(folderPath);
+//        File[] listOfFiles = folder.listFiles();
+//
+//        for (int i = 0; i < listOfFiles.length; i++) {
+//            if (listOfFiles[i].isFile()) {
+//                System.out.println("File " + listOfFiles[i].getName());
+//                BufferedReader br = null;
+//                try {
+//                    br = new BufferedReader(new FileReader(folderPath+"/"+listOfFiles[i].getName()));
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//                String line=null;
+//                int correct=0;
+//                int total=0;
+//                try {
+//                    while ( (line=br.readLine()) != null) {
+//                        String predict_label = languageDetector.predict(line, 2);
+//                        if (predict_label.equals(listOfFiles[i].getName()))
+//                            correct++;
+//                        else {
+//                            System.out.println(line);
+//                            System.out.println(predict_label);
+//                        }
+//                        total++;
+//                    }
+//                    System.out.println("Accuracy: "+(double)correct/total);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            } else if (listOfFiles[i].isDirectory()) {
+//                System.out.println("Directory " + listOfFiles[i].getName());
+//            }
+//        }
+//    }
 }
